cd6e65dc8769623c6e3cdddf34f790fb5401dfff
Will Hart <hart.wl@gmail.com>
2014-06-14 17:05:23 +0100

    New mission map selection dialog

---
+++ AnvilEditor/NewMissionDialog.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using AnvilEditor.Models;
+
+namespace AnvilEditor
+{
+    /// <summary>
+    /// Interaction logic for NewMissionDialog.xaml
+    /// </summary>
+    public partial class NewMissionDialog : Window
+    {
+        public NewMissionDialog()
+        {
+            InitializeComponent();
+
+            // load up the mission names
+            foreach (var map in MapDefinitions.MapNames)
+            {
+                this.MapListBox.Items.Add(map);
+            }
+
+            // select Altis
+            this.MapListBox.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Update the credits box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MapListBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.SelectedMapName = this.MapListBox.SelectedValue.ToString();
+            this.MapDetailsTextBox.Text = MapDefinitions.Maps[this.SelectedMapName].ToString();
+        }
+
+        /// <summary>
+        /// Close the dialog
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        /// <summary>
+        /// Gets the map name selected by the dialog
+        /// </summary>
+        public string SelectedMapName { get; private set; }
+
+    }
+}
