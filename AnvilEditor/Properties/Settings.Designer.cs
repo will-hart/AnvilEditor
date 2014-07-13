17dd4075cbcb7c93b9499a807d2500bc6cabd216
Will Hart <hart.wl@gmail.com>
2014-07-13 14:34:39 +0100

    Bump version number

--- AnvilEditor/Properties/Settings.Designer.cs
+++ AnvilEditor/Properties/Settings.Designer.cs
@@ -48,7 +48,7 @@ namespace AnvilEditor.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("4")]
         public int FrameworkVersion {
             get {
                 return ((int)(this["FrameworkVersion"]));
