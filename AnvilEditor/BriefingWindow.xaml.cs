1ae47314b0d7fab9d9931b41584790511da7ae5b
Will Hart <hart.wl@gmail.com>
2014-08-14 08:01:13 +0100

    Implement briefing editor

--- AnvilEditor/BriefingWindow.xaml.cs
+++ AnvilEditor/BriefingWindow.xaml.cs
@@ -1,38 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 using AnvilEditor.Models;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace AnvilEditor
 {
     /// <summary>
     /// Interaction logic for BriefingWindow.xaml
     /// </summary>
-    public partial class BriefingWindow : Window
+    public partial class BriefingWindow
     {
+        /// <summary>
+        /// The mission whose briefing is being edited
+        /// </summary>
         private Mission mission;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="mission"></param>
         public BriefingWindow(Mission mission)
         {
             this.mission = mission;
             this.DataContext = this.mission;
+            this.SelectedSection = "";
             InitializeComponent();
+            this.UpdateSections();
+            this.UpdateDetailView(this.SelectedSection);
         }
 
-        private void AddBriefingItemButtonClick(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Shows a prompt for a new section name and adds it to the end
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void AddBriefingItemButtonClick(object sender, RoutedEventArgs e)
         {
-            
+            var title = await this.ShowInputAsync("Specify section name", "Enter a briefing section name");
+            if (title.Length == 0) return;
+
+            if (!this.mission.MissionBriefing.BriefingSections.Contains(title))
+            {
+                this.mission.MissionBriefing.BriefingSections.Add(title);
+            }
+
+            this.UpdateSections();
         }
+
+        /// <summary>
+        /// Deletes a briefing item from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteBriefingItemButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.SelectedSection == "") return;
+
+            this.mission.MissionBriefing.Delete(this.SelectedSection);
+        }
+
+        /// <summary>
+        /// Updates the sections in the list box
+        /// </summary>
+        private void UpdateSections()
+        {
+            this.SectionListBox.Items.Clear();
+            foreach (var s in this.mission.MissionBriefing.BriefingSections)
+            {
+                this.SectionListBox.Items.Add(s);
+            }
+
+            if (this.SelectedSection != "")
+            {
+                this.SectionListBox.SelectedItem = this.SelectedSection;
+                this.UpdateDetailView(this.SelectedSection);
+            }
+        }
+
+        /// <summary>
+        /// Handle selecting a new item in the list box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SectionListBoxSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (this.SectionListBox.SelectedIndex == -1)
+            {
+                this.SelectedSection = "";
+            }
+            else
+            {
+                this.SelectedSection = (string)this.SectionListBox.SelectedItem;
+            }
+
+            this.UpdateDetailView(this.SelectedSection);
+        }
+
+        /// <summary>
+        /// Sets the detail view to match the selected section
+        /// </summary>
+        /// <param name="p"></param>
+        private void UpdateDetailView(string p)
+        {
+            this.SectionTextBox.Text = this.mission.MissionBriefing.Get(p);
+        }
+
+        /// <summary>
+        /// Poor man's data binding
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SectionTextBoxTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (this.SelectedSection != "")
+            {
+                this.mission.MissionBriefing.Set(this.SelectedSection, this.SectionTextBox.Text);
+            }
+        }
+      
+        /// <summary>
+        /// The string section heading currently selected in the LH box
+        /// </summary>
+        private string SelectedSection { get; set; }
     }
 }
--- AnvilEditor/Models/Briefing.cs
+++ AnvilEditor/Models/Briefing.cs
@@ -9,9 +9,90 @@ namespace AnvilEditor.Models
 {
     public class Briefing : ObservableCollection<string>
     {
+        internal Briefing()
+        {
+            this.BriefingParts = new Dictionary<string, string>();
+            this.BriefingSections = new List<string>();
+        }
+
         /// <summary>
         /// The briefing sections that are displayed in the mission editor
         /// </summary>
-        public Dictionary<string, string> BriefingParts { get; set; }
+        internal Dictionary<string, string> BriefingParts { get; set; }
+
+        /// <summary>
+        /// The ordered sections of the briefing. THis is the main source of briefing sections
+        /// </summary>
+        internal List<string> BriefingSections { get; set; }
+
+        /// <summary>
+        /// Provides a string output of the briefing
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var output = @"waitUntil { !isNil {player} };
+waitUntil { player == player };
+
+";
+            var tpl = "player createDiaryRecord [\"Diary\", [\"{0}\", \"{1}\"]];";
+
+            foreach (var s in this.BriefingSections)
+            {
+                output += string.Format(tpl, s, this.BriefingParts.ContainsKey(s) ? this.BriefingParts[s] : "") + Environment.NewLine;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Deletes a section from the briefing
+        /// </summary>
+        /// <param name="p">The name of the section to delete</param>
+        internal void Delete(string p)
+        {
+            this.BriefingSections.Remove(p);
+            this.BriefingParts.Remove(p);
+        }
+
+        /// <summary>
+        /// Adds or updates a section to the briefing
+        /// </summary>
+        /// <param name="p"></param>
+        internal void Set(string key, string value="")
+        {
+            if (key == "")
+            {
+                return;
+            }
+
+            if (!this.BriefingSections.Contains(key))
+            {
+                this.BriefingSections.Add(key);
+            }
+
+            if (!this.BriefingParts.ContainsKey(key))
+            {
+                this.BriefingParts.Add(key, value);
+            }
+            else
+            {
+                this.BriefingParts[key] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets a briefing section or returns an empty string if the key is unknown
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        internal string Get(string key)
+        {
+            if (!this.BriefingParts.ContainsKey(key) || key == "")
+            {
+                return "";
+            }
+            return this.BriefingParts[key];
+        }
     }
 }
