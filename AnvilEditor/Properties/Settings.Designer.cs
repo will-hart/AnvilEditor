13a0ace87def1fb6e74e89801b8d1a875eefb3ef
Will Hart <hart.wl@gmail.com>
2014-12-21 15:40:17 +0000

    Bump assembly version

--- AnvilEditor/Properties/Settings.Designer.cs
+++ AnvilEditor/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AnvilEditor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -48,7 +48,7 @@ namespace AnvilEditor.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("4")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5")]
         public int FrameworkVersion {
             get {
                 return ((int)(this["FrameworkVersion"]));
