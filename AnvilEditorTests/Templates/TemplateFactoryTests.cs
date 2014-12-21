c7bea8be669b12a62243b48555271966cc9e7c24
Will Hart <hart.wl@gmail.com>
2014-12-21 12:39:30 +0000

    Template Factory tests

--- AnvilEditorTests/Templates/TemplateFactoryTests.cs
+++ AnvilEditorTests/Templates/TemplateFactoryTests.cs
@@ -1,19 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AnvilEditor.Templates;
-using NUnit.Framework;
-namespace AnvilEditor.Templates.Tests
+﻿namespace AnvilEditor.Templates.Tests
 {
+    using NUnit.Framework;
+
+    using AnvilEditor.Templates;
+    using AnvilParser;
+    using AnvilEditor.Models;
+    using System.Collections.Generic;
+
     [TestFixture()]
     public class TemplateFactoryTests
     {
         [Test()]
-        public void MissionTest()
+        public void MissionTemplateShouldGenerateRootMissionBase()
         {
-            Assert.Fail();
+            var tpl = TemplateFactory.Mission();
+
+            Assert.NotNull(tpl.GetClass("Mission"));
+            Assert.NotNull(tpl.GetClass("Mission.Groups"));
+            Assert.NotNull(tpl.GetClass("Mission.Intel"));
+            Assert.NotNull(tpl.GetToken("Mission.Addons"));
+            Assert.NotNull(tpl.GetToken("Mission.addOnsAuto"));
+            Assert.NotNull(tpl.GetToken("Mission.randomSeed"));
+
+            Assert.NotNull(tpl.GetClass("Intro"));
+            Assert.NotNull(tpl.GetClass("Intro.Intel"));
+            Assert.NotNull(tpl.GetToken("Intro.Addons"));
+            Assert.NotNull(tpl.GetToken("Intro.addOnsAuto"));
+            Assert.NotNull(tpl.GetToken("Intro.randomSeed"));
+
+            Assert.NotNull(tpl.GetClass("OutroLoose"));
+            Assert.NotNull(tpl.GetClass("OutroLoose.Intel"));
+            Assert.NotNull(tpl.GetToken("OutroLoose.Addons"));
+            Assert.NotNull(tpl.GetToken("OutroLoose.addOnsAuto"));
+            Assert.NotNull(tpl.GetToken("OutroLoose.randomSeed"));
+
+            Assert.NotNull(tpl.GetClass("OutroWin"));
+            Assert.NotNull(tpl.GetClass("OutroWin.Intel"));
+            Assert.NotNull(tpl.GetToken("OutroWin.Addons"));
+            Assert.NotNull(tpl.GetToken("OutroWin.addOnsAuto"));
+            Assert.NotNull(tpl.GetToken("OutroWin.randomSeed"));
         }
 
         [Test()]
@@ -23,27 +48,82 @@ namespace AnvilEditor.Templates.Tests
         }
 
         [Test()]
-        public void MarkerTest()
+        public void MarkerTemplateAppliesCustomValues()
         {
-            Assert.Fail();
+            var mkr_defaults = TemplateFactory.Marker(10, 20, "test_marker", "ColorRed", "test marker");
+            
+            Assert.AreEqual("Item000_test_marker", mkr_defaults.Name);
+            Assert.AreEqual("[ 10, 0.0, 20]", mkr_defaults.GetToken("position").ToString());
+            Assert.AreEqual("\"test_marker\"", mkr_defaults.GetToken("name").ToString());
+            Assert.AreEqual("\"Empty\"", mkr_defaults.GetToken("type").ToString());
+            Assert.AreEqual("\"ColorRed\"", mkr_defaults.GetToken("colorName").ToString());
+            Assert.AreEqual("\"test marker\"", mkr_defaults.GetToken("text").ToString());
+        }
+
+        [Test()]
+        public void MarkerSetsCorrectDefaults()
+        {
+            var mkr_defaults = TemplateFactory.Marker(10, 20, "test_marker");
+
+
+            Assert.AreEqual("Item000_test_marker", mkr_defaults.Name);
+            Assert.AreEqual("[ 10, 0.0, 20]", mkr_defaults.GetToken("position").ToString());
+            Assert.AreEqual("\"test_marker\"", mkr_defaults.GetToken("name").ToString());
+            Assert.AreEqual("\"Empty\"", mkr_defaults.GetToken("type").ToString());
+            Assert.AreEqual("\"ColorOrange\"", mkr_defaults.GetToken("colorName").ToString());
+            Assert.AreEqual("\"\"", mkr_defaults.GetToken("text").ToString());
         }
 
         [Test()]
         public void CompleteObjectiveTriggerTest()
         {
-            Assert.Fail();
+            var aot = TemplateFactory.CompleteObjectiveTrigger(1, EndTriggerTypes.END2);
+
+            var expectedCondition = "server getVariable \"\"objective_1\"\"";
+
+            Assert.AreEqual(aot.Name, "Item0_000_1");
+            Assert.AreEqual(aot.GetToken("type").Value.ToString(), "END2");
+            Assert.AreEqual(aot.GetToken("name").Value.ToString(), "fw_trig_obj1");
+            Assert.AreEqual(aot.GetToken("expCond").Value.ToString(), expectedCondition);
+            Assert.AreEqual(aot.GetToken("timeoutMin").Value.ToString(), "10");
+            Assert.AreEqual(aot.GetToken("timeoutMid").Value.ToString(), "10");
+            Assert.AreEqual(aot.GetToken("timeoutMax").Value.ToString(), "10");
         }
 
         [Test()]
-        public void AllObjectivesTriggerTest()
+        public void AllObjectivesTriggerShouldBuildCorrectParserClass()
         {
-            Assert.Fail();
+            var aot = TemplateFactory.AllObjectivesTrigger("END1");
+
+            Assert.AreEqual(aot.Name, "Item0_000_all");
+            Assert.AreEqual(aot.GetToken("type").Value.ToString(), "END1");
+            Assert.AreEqual(aot.GetToken("name").Value.ToString(), "fw_trig_obj_all");
+            Assert.AreEqual(aot.GetToken("expCond").Value.ToString(), "all_objectives_complete");
+            Assert.AreEqual(aot.GetToken("timeoutMin").Value.ToString(), "10");
+            Assert.AreEqual(aot.GetToken("timeoutMid").Value.ToString(), "10");
+            Assert.AreEqual(aot.GetToken("timeoutMax").Value.ToString(), "10");
         }
 
         [Test()]
         public void KeyObjectivesTriggerTest()
         {
-            Assert.Fail();
+            var objs = new List<Objective>()
+            {
+                new Objective(1, new System.Windows.Point(0, 0)),
+                new Objective(2, new System.Windows.Point(0, 0))
+            };
+            var aot = TemplateFactory.KeyObjectivesTrigger("END1", objs);
+
+            var expectedCondition = "(server getVariable \"\"objective_1\"\") and (server getVariable \"\"" +
+                "objective_2\"\")";
+
+            Assert.AreEqual(aot.Name, "Item0_000_key");
+            Assert.AreEqual(aot.GetToken("type").Value.ToString(), "END1");
+            Assert.AreEqual(aot.GetToken("name").Value.ToString(), "fw_trig_obj_key");
+            Assert.AreEqual(aot.GetToken("expCond").Value.ToString(), expectedCondition);
+            Assert.AreEqual(aot.GetToken("timeoutMin").Value.ToString(), "10");
+            Assert.AreEqual(aot.GetToken("timeoutMid").Value.ToString(), "10");
+            Assert.AreEqual(aot.GetToken("timeoutMax").Value.ToString(), "10");
         }
 
         [Test()]
