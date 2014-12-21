4564875ad385240336f66d912cbe0b4afa48c983
Will Hart <hart.wl@gmail.com>
2014-12-21 13:56:54 +0000

    Unit test ouput generator mission lint checks

--- AnvilEditorTests/OutputGeneratorTests.cs
+++ AnvilEditorTests/OutputGeneratorTests.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AnvilEditor;
-using NUnit.Framework;
-namespace AnvilEditor.Tests
+﻿namespace AnvilEditor.Tests
 {
+    using NUnit.Framework;
+    using System.Windows;
+
+    using AnvilEditor.Models;
+
     [TestFixture()]
     public class OutputGeneratorTests
     {
@@ -17,9 +15,47 @@ namespace AnvilEditor.Tests
         }
 
         [Test()]
-        public void CompleteChecksTest()
+        public void CompleteChecksShouldIdentifyOverPopulatedObjectives()
         {
-            Assert.Fail();
+            var m = new Mission();
+            var o = m.AddObjective(new Point(10, 10));
+            o.Infantry = 1000;
+            o.Radius = 50;
+
+            var result = OutputGenerator.CompleteChecks(m);
+
+            Assert.IsTrue(result.Contains("Occupation of objective 0"));
+        }
+
+        [Test()]
+        public void CompleteChecksShouldIdentifySameFriendlyAndEnemySide()
+        {
+            var m = new Mission();
+            m.FriendlySide = "EAST";
+            m.EnemySide = "EAST";
+            var result = OutputGenerator.CompleteChecks(m);
+
+            Assert.IsTrue(result.Contains("The friendly and enemy side are the same"));
+        }
+
+        [Test()]
+        public void CompleteChecksShouldIdentifyUnoccupiedObjectives()
+        {
+            var m = new Mission();
+            var o = m.AddObjective(new Point(10, 10));
+            var result = OutputGenerator.CompleteChecks(m);
+
+            Assert.IsTrue(result.Contains("There are 1 unoccupied objective"));
+        }
+
+        [Test()]
+        public void CompleteChecksShouldIdentifyUnoccupiedAmbientZones()
+        {
+            var m = new Mission();
+            var z = m.SetAmbientZone(new Point(1, 1));
+            var result = OutputGenerator.CompleteChecks(m);
+
+            Assert.IsTrue(result.Contains("There are 1 unoccupied ambient zone"));
         }
     }
 }
