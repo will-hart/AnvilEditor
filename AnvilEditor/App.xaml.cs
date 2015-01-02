8342484f09d44969d9e7fc0e4aca00e14bfaa98e
Will Hart <hart.wl@gmail.com>
2015-01-02 16:19:33 +0000

    Generic catch for exceptions when logging exceptions to sentry

--- AnvilEditor/App.xaml.cs
+++ AnvilEditor/App.xaml.cs
@@ -37,20 +37,35 @@
             this.CaptureException(e.ExceptionObject as Exception);
         }
 
+        /// <summary>
+        /// Logs the exception through Raven/Sentry. Wraps in generic try-catch to prevent an endless exception loop
+        /// </summary>
+        /// <param name="e"></param>
         private void CaptureException(Exception e)
         {
-            var result = MessageBox.Show("The application has experienced an error and will probably have to close. Would you like to send an anonymous error report to the developer? " +
-                Environment.NewLine + Environment.NewLine + "If you do not wish to report the error, click 'NO'. You may still like to check the /logs directory and bring the contents " + 
-                "and a description of the error to the attention of the developer through the BI forums.", "Error! Do you want to report it?",
-                MessageBoxButton.YesNo, MessageBoxImage.Error);
-            
-            if (result == MessageBoxResult.Yes)
+            try
             {
-                var ravenClient = new RavenClient("https://4510c8a1199545abb190d5e63d15885f:f0ca1031909b49899ee4b0898a397826@anvilsentry.herokuapp.com/2");
-                ravenClient.CaptureException(e);
-            }
+                var result = MessageBox.Show("The application has experienced an error and will probably have to close. Would you like to send an anonymous error report to the developer? " +
+                    Environment.NewLine + Environment.NewLine + "If you do not wish to report the error, click 'NO'. You may still like to check the /logs directory and bring the contents " +
+                    "and a description of the error to the attention of the developer through the BI forums.", "Error! Do you want to report it?",
+                    MessageBoxButton.YesNo, MessageBoxImage.Error);
 
-            Application.Current.Shutdown();
+                if (result == MessageBoxResult.Yes)
+                {
+                    var ravenClient = new RavenClient("https://4510c8a1199545abb190d5e63d15885f:f0ca1031909b49899ee4b0898a397826@anvilsentry.herokuapp.com/2");
+                    ravenClient.CaptureException(e);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("EXCEPTION OCURRED WHILST TRY TO REPORT AN EXCEPTION! OH DEAR!");
+                Log.Error(ex.Message);
+                Log.Error(ex.StackTrace);
+            }
+            finally
+            {
+                Application.Current.Shutdown();
+            }
         }
     }
 }
